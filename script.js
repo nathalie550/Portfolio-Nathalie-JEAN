5522dba42cd5056554abe849f91c345fb174e89e
Nathalie <jeannathalie550@gmail.com>
2025-12-09 16:01:47 +0100

    Ajout fichiers

--- script.js
+++ script.js
@@ -3,3 +3,16 @@
 function goBack() {
     window.history.back();
 }
+
+
+/********************FICHIER METIERS AVEC DETAILS  POPUP********************************/
+
+function openDetail(id) {
+    document.getElementById(id).style.display = "block";
+}
+
+function closeDetail(id) {
+    document.getElementById(id).style.display = "none";
+}
+
+
